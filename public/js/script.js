60d222266e904deb6c7601ce5a8ca5a84a24105e
Nic Santana <nick.santana@gmail.com>
2018-01-17 18:37:00 -0500

    Initial commit

---
+++ public/js/script.js
@@ -0,0 +1,18 @@
+// Drum Arrays
+/* let kicks = [false,false,false,false,false,false,false,false,false,false,false,false,false,false, false,false];
+let snares
+let hiHats
+let rideCymbals */
+
+
+const kicks = []
+const snares = []
+const hiHats = []
+const rideCymbals = []
+
+for (let i = 0; i < 16; i++) {
+  kicks[i] = false;
+  snares[i] = false;
+  hiHats[i] = false;
+  rideCymbals[i] = false;
+}
